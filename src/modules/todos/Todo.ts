9ef30717d85c19b4aa402941783c87fa2a920075
Lilian Nguyen <24741666+lilian-n@users.noreply.github.com>
2022-10-18 17:35:41 -0600

    Add create method to Todo

--- src/modules/todos/Todo.ts
+++ src/modules/todos/Todo.ts
@@ -1,3 +1,6 @@
+import { TextUtil } from "../../shared/utils/TextUtil.ts";
+import { Result } from "../../shared/logic/Result.ts";
+
 type TodoProps = {
   id?: string;
   content: string;
@@ -15,13 +18,26 @@ export class Todo {
     this.props.isCompleted = props.isCompleted ? props.isCompleted : false;
   }
 
-  public static create(content: string) {}
-
   public getContent() {
     return this.props.content;
   }
 
   public isCompleted() {
     return this.props.isCompleted;
+  }
+
+  public static create(content: string) {
+    const isValidLength =
+      TextUtil.isAtLeast(5, content) && TextUtil.isAtMost(30, content);
+
+    if (!isValidLength) {
+      return Result.fail<Todo>("Must be between 5 to 30 characters");
+    }
+
+    const args = {
+      content: content,
+    };
+
+    return Result.ok<Todo>(new Todo(args));
   }
 }
